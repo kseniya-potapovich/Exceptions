be83164a8bd2a3344f05b9f8c092ad4d5a798093
Kseniya Potapovich <ksuy.potapovich@bk.ru>
2023-11-21 06:42:04 +0300

    Second commit

--- src/Person.java
+++ src/Person.java
@@ -3,19 +3,23 @@ public class Person {
     private String password;
     private String confirmPassword;
 
-
-    static void methodOfPassword(String login, String password, String confirmPassword) {
+    static public boolean method(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
         try {
-            if (login.length() <= 20 && login.replaceAll(" ", "") == login) {
+            if (login.length() <= 20 && login.isBlank()) {
                 System.out.println("Login is correct");
+            } else {
+                throw new WrongLoginException();
             }
-            if ((password.length() <= 20 && password.equals(confirmPassword)) == true) {
+            if (password.length() <= 20 && password.isBlank() && password.equals(confirmPassword)) {
                 System.out.println("Password is correct");
+            } else {
+                throw new WrongPasswordException();
             }
+            return true;
         } catch (WrongLoginException e) {
-            System.out.println("Login isn't correct");
+            throw new WrongLoginException();
         } catch (WrongPasswordException e) {
-            System.out.println("Password isn't correct");
+            throw new WrongPasswordException();
         }
     }
 }
